4a719e275c3639b2a2186711d9d616ce9435d614
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-11-05 12:30:56 +1100

    Update agent for environment

--- reinforcement-learning/play.py
+++ reinforcement-learning/play.py
@@ -1,29 +1,7 @@
 """This is the agent which currently takes the action with highest immediate reward."""
 
-import pandas as pd
-import numpy as np
 import env
-
-actions = ["left", "right", "stay"]
-
-left = {x: [0]*(env.screen_width - 1) for x in range(2)}
-right = {x: [0]*(env.screen_width - 1) for x in range(2)}
-table = pd.DataFrame(left)
-
-def max(list):
-    max = 0
-    index = 0
-    for item in list:
-        item += 1
-        if item > max:
-            max = item
-    return item
-
-
-"""if np.random.uniform() > epsilon or all_zero:
-    action = np.random.choice(actions)
-else:
-    action = None"""
+env.make("text")
 
 for episode in range(10):
     env.reset()
@@ -37,7 +15,7 @@ for episode in range(10):
             break
         max_action = 0
         index = -1
-        for item in actions:
+        for item in env.actions:
             if env.create_reward(item) > max_action:
                 max_action = env.create_reward(item)
                 action = [item, index]

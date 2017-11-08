0193333eeae5495227c035683fe93f62d051b54a
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-11-08 19:28:45 +1100

    Update choose_action function.

--- reinforcement-learning/rl.py
+++ reinforcement-learning/rl.py
@@ -6,7 +6,7 @@ import numpy as np
 import env
 
 actions = ["left", "right", "stay"]
-gamma = 0.9
+gamma = 0.8 # make the agent explore a bit
 table = np.zeros((28, 28, 3))
 
 
@@ -22,6 +22,7 @@ def all_zero(list):
 def choose_action(state):
     """Choose action based on q table."""
     if np.random.uniform() > gamma or all_zero(state):
+    #if all_zero(state):
         action = np.random.choice(actions)
         print("zero")
         return action
@@ -31,12 +32,17 @@ def choose_action(state):
         index_1 = int(round(action / 3))
         index_2 = int(action - index_1 * 3)
         index_1 -= 1
-        index_2 -= 1
-        action = state[index_1][index_2]
+        if index_1 == 0:
+            action = "left"
+        elif index_1 == 1:
+            action = "right"
+        else:
+            action = "stay"
         return action
 
 def q(state, action):
     """Update the q table."""
+    print("[+] INFO object:", env.object[0], "player:", env.player)
     index = -1
     for item in actions:
         index += 1
@@ -44,9 +50,8 @@ def q(state, action):
             action_n = index
 
     # q learning
-    new_value = reward(action) + gamma * np.amax(table[state])
+    new_value = reward(action) + gamma * np.amax(table[env.object[0]])
 
-    print(new_value)
-    table[env.object[0]][state][action_n] = new_value
-    print("special")
-    print(table[env.object[0]][state])
+    print(new_value, state, action)
+    print(table[env.object[0]][int(state)])
+    table[env.object[0]][int(state)][action_n] = new_value

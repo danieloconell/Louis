dcc20bef85d0f57d066dd5734ca672045e8a1ab9
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-11-08 16:26:05 +1100

    Working choose_action function.

--- reinforcement-learning/rl.py
+++ reinforcement-learning/rl.py
@@ -7,14 +7,15 @@ import numpy as np
 import env
 
 actions = ["left", "right", "stay"]
-gamma = 0.8
-table = np.zeros((14, 3, 28))
+gamma = 0.9
+table = np.zeros((28, 3, 28))
 
 
 def all_zero(list):
     for item in list:
-        if item != 0:
-            return False
+        for x in item:
+            if x != 0:
+                return False
     return True
 
 
@@ -23,11 +24,15 @@ def choose_action(state):
     if np.random.uniform() > gamma or all_zero(state):
         action = np.random.choice(actions)
         print("zero")
-        print(action)
         return action
     else:
-        action = max(state)
-        print(action)
+        action = np.argmax(state)
+        action += 1
+        index_1 = int(round(action / 3))
+        index_2 = int(action - index_1 * 3)
+        index_1 -= 1
+        index_2 -= 1
+        action = state[index_1][index_2]
         return action
 
 def q(state, action):
@@ -39,7 +44,5 @@ def q(state, action):
             action_n = index
 
     # q learning
-    new_value = reward(action) + gamma * max(table[action_n][env.object[0]][(state - 1)], table[action_n][env.object[0]][(state)], table[action_n][env.object[0]][(state + 1)])
-    table[env.player][env.object[0]][state] = new_value
     print(new_value)
-    np.save("q-table", table)
+    table[env.object[0]][action_n][state] = new_value

cd8fe432077bdd65122189dd9191d7a5b8788e48
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-11-09 11:54:12 +1100

    Use proper q learning for agent.

--- reinforcement-learning/play.py
+++ reinforcement-learning/play.py
@@ -1,10 +1,12 @@
 """This is the agent which currently takes the action with highest immediate reward."""
 
-import env
 import time
-env.make("pygame")
+start = time.time()
+import env
+import rl
+env.make("text")
 
-for episode in range(10):
+for episode in range(1000):
     env.reset()
     episode_reward = 0
     for t in range(100):
@@ -14,18 +16,11 @@ for episode in range(10):
                 "Episode %d finished after %d timesteps, with reward %d"
                 % ((episode + 1), (t + 1), episode_reward))
             break
-        max_action = -1
-        index = -1
-        for item in env.actions:
-            print(item)
-            print(env.reward(item))
-            if env.reward(item) > max_action:
-                print("greater")
-                max_action = env.reward(item)
-                action = [item, index]
-            else:
-                index += 1
-        print(action[0])
-        episode_reward += env.reward(action[0])
-        env.action(action[0])
-        env.render()
+        action = rl.choose_action(rl.table[env.object[0]])
+        rl.q(env.player, action)
+        print(action)
+        episode_reward += env.reward(action)
+        env.action(action)
+        env.update()
+print(rl.table[env.object[0]])
+print("Finished after", str(time.time() - start), "seconds")

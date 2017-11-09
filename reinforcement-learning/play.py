24bf1aba15f96b3aafae2f395052ca3de6810b78
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-11-09 18:15:33 +1100

    Initial play.py.

---
+++ reinforcement-learning/play.py
@@ -0,0 +1,19 @@
+"""Load the trained q table and make actions based on that.
+"""
+
+import time
+import env
+import rl
+
+rl.load_q()
+env.make("pygame")
+
+while True:
+    env.reset()
+    for _ in range(15):
+        if env.done:
+            break
+        action = rl.choose_action(rl.table[env.object[0]])
+        env.action(action)
+        time.sleep(0.03)
+        env.render()

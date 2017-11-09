47664e537faf7b68add77f7f0c7a146cae419051
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-11-09 16:37:23 +1100

    Add progress bar.

--- reinforcement-learning/train.py
+++ reinforcement-learning/train.py
@@ -2,27 +2,27 @@
 
 import time
 start = time.time()
+from tqdm import tqdm
 import env
 import rl
 env.make("text")
 rl.load_q()
+episodes = 10000
 
-for episode in range(10000):
-    env.reset()
-    episode_reward = 0
-    for t in range(100):
-        episode_reward += env.actual_reward
-        if env.done:
-            print(
-                "Episode %d finished after %d timesteps, with reward %d"
-                % ((episode + 1), (t + 1), episode_reward))
-            break
-        action = rl.choose_action(rl.table[env.object[0]])
-        rl.q(env.player, action)
-        print(action)
-        episode_reward += env.reward(action)
-        env.action(action)
-        env.update()
+with tqdm(total=episodes) as pbar:
+    for episode in range(episodes):
+        env.reset()
+        episode_reward = 0
+        for t in range(100):
+            episode_reward += env.actual_reward
+            if env.done:
+                pbar.update(1)
+                break
+            action = rl.choose_action(rl.table[env.object[0]])
+            rl.q(env.player, action)
+            episode_reward += env.reward(action)
+            env.action(action)
+            env.update()
 rl.save_q()
+print("Q table:")
 print(rl.table[env.object[0]])
-print("Finished after", str(time.time() - start), "seconds")

94b62a5c0d3d47b9eccc7d5fe0f07bc86b978278
Joel Bryla <joel@wiredlizard.com>
2017-11-11 19:27:14 +1100

    Added loading with different architecture

--- music/lstm.py
+++ music/lstm.py
@@ -14,7 +14,7 @@ if args.Action.lower() != "create" and args.Action.lower() != "learn":
     quit()
 
 import numpy
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import LSTM
@@ -61,16 +61,17 @@ X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
 X = X / float(n_vocab)
 y = np_utils.to_categorical(dataY)
 
-model = Sequential()
-model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(256))
-model.add(Dropout(0.2))
-model.add(Dense(y.shape[1], activation='softmax'))
-if args.weights:
-    model.load_weights(args.weights)
 
-model.compile(loss='categorical_crossentropy', optimizer='adam')
+if args.weights:
+    model = load_model(args.weights)
+else:
+    model = Sequential()
+    model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2]), return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(256))
+    model.add(Dropout(0.2))
+    model.add(Dense(y.shape[1], activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 
 def learn():

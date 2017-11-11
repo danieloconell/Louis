1b248df002f6a9c207f74c9a8125ff88ec411ccb
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-11-11 12:50:40 +1100

    Add speed to variable.

--- reinforcement-learning/play.py
+++ reinforcement-learning/play.py
@@ -7,13 +7,25 @@ import rl
 
 rl.load_q()
 env.make("pygame")
+speed = 0.2
 
 while True:
+    if speed >= 0.02:
+        speed -= 0.05
+    elif speed <= 0.005 and speed >= 0.05:
+        print("second")
+        speed -= 0.005
+    elif speed <= 0.005 and speed > 0.0005:
+        print("third")
+        speed -= 0.0005
+    else:
+        speed = 0
+    print(speed)
     env.reset()
     for _ in range(15):
         if env.done:
             break
         action = rl.choose_action(env.player, "test")
         env.action(action)
-        time.sleep(0.03)
+        time.sleep(speed)
         env.render()

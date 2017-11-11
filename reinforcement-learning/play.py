87e3954d64ba07111b10a8905639336f3002e48e
Daniel O'Connell <dan.ben.oconnell@gmail.com>
2017-11-11 13:26:35 +1100

    Remove unnecessary print statements.

--- reinforcement-learning/play.py
+++ reinforcement-learning/play.py
@@ -13,14 +13,11 @@ while True:
     if speed >= 0.02:
         speed -= 0.05
     elif speed <= 0.005 and speed >= 0.05:
-        print("second")
         speed -= 0.005
     elif speed <= 0.005 and speed > 0.0005:
-        print("third")
         speed -= 0.0005
     else:
         speed = 0
-    print(speed)
     env.reset()
     for _ in range(15):
         if env.done:
